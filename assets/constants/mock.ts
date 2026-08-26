287ddef18c2139a49473c7794f8f826e07f95b7d
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-08-26 12:27:20 -0300

    Add files via upload

---
+++ assets/constants/mock.ts
@@ -0,0 +1,22 @@
+const data = [
+    {
+        "id": "1",
+        "title": "ROBÓTICA",
+        "text": "A robótica é um ramo da tecnologia que une a mecânica, a eletrônica e a programação para criar máquinas automáticas capazes de realizar tarefas.",
+        "image": require('@/assets/images/auto.png')
+    },
+    {
+        "id": "2",
+        "title": "SISTEMAS",
+        "text": "O desenvolvimento de sistemas é a área da tecnologia responsável por planejar, criar, testar e manter softwares e aplicações digitais.",
+        "image": require('@/assets/images/ds.png')
+    },
+    {
+        "id": "3",
+        "title": "LOGÍSTICA",
+        "text": "A logística é o processo de planejar, executar e controlar o fluxo eficiente de materiais, produtos e informações desde o ponto de origem até o consumidor final.",
+        "image": require('@/assets/images/log.png')
+    }
+]
+
+export default data
